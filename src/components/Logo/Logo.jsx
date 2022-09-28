30d5e98499b9d605b2ca4cd29505901220afbfb6
ianchan3 <ianchan3138@gmail.com>
2022-09-28 16:23:58 -0700

    Create Logo

---
+++ src/components/Logo/Logo.jsx
@@ -0,0 +1,9 @@
+import './Logo.css';
+
+export default function Logo() {
+  return (
+    <div className="Logo">
+      <h1>IC</h1>
+    </div>
+  );
+}
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,15 +1,14 @@
 import { Link } from 'react-router-dom';
 import './NavBar.css';
+import Logo from '../Logo/Logo';
+
 
 export default function NavBar() {
   return (
     <main className="NavBar">
       <nav>
-        
-        <Link to ="/">Home</Link>
-        
+        <Link to ="/"><Logo /></Link>
         <Link to ="/about">About Me</Link>
-
         <Link to ="/projects">Projects</Link>
       </nav>
     </main>
