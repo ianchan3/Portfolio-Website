c82a0ad0e3baf82f6b79855ce1993cd7b24c6a14
ianchan3 <ianchan3138@gmail.com>
2022-09-29 11:52:28 -0700

    Attempt to Debug Link Not Working on Deployment Website but Working in localhost

--- src/pages/ProjectPage/ProjectPage.jsx
+++ src/pages/ProjectPage/ProjectPage.jsx
@@ -1,4 +1,5 @@
 import "./ProjectPage.css";
+import {Link} from "react-router-dom";
 
 
 export default function ProjectPage () {
@@ -6,10 +7,10 @@ export default function ProjectPage () {
     <main className="ProjectPage">
       <h1>Projects</h1>
       <p>
-      <a href="/projects/ConnectFour"><img src="https://i.imgur.com/ZaLPJld.jpg" alt="Connect Four"></img></a>
-      <a href="/projects/FeelTheHeat"><img src="https://i.imgur.com/hGDaUCp.jpg" alt="Feel The Heat?"></img></a>
-      <a href="/projects/LoveStruck"><img src="https://i.imgur.com/W3Mikdl.png" alt="LoveStruck"></img></a>
-      <a href="/projects/GrowGreen"><img src="https://i.imgur.com/PCvd6LZ.png" alt="Grow Green"></img></a>
+      <Link link to="/projects/ConnectFour"><img src="https://i.imgur.com/ZaLPJld.jpg" alt="Connect Four"></img></Link>
+      <Link link to="/projects/FeelTheHeat"><img src="https://i.imgur.com/hGDaUCp.jpg" alt="Feel The Heat?"></img></Link>
+      <Link link to="/projects/LoveStruck"><img src="https://i.imgur.com/W3Mikdl.png" alt="LoveStruck"></img></Link>
+      <Link link to="/projects/GrowGreen"><img src="https://i.imgur.com/PCvd6LZ.png" alt="Grow Green"></img></Link>
      
       </p>
     </main>
