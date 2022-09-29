380c82d37e9622deea78338644677fddf6fcf8bb
ianchan3 <ianchan3138@gmail.com>
2022-09-29 10:18:41 -0700

    Connect Four GitHub Link and Deployment Link CSS

--- src/pages/ConnectFourPage/ConnectFourPage.jsx
+++ src/pages/ConnectFourPage/ConnectFourPage.jsx
@@ -3,11 +3,17 @@ import "./ConnectFourPage.css"
 export default function ConnectFourPage() {
   return (
     <main class="ConnectFourPage">
+      <link href="https://cdn.jsdelivr.net/npm/remixicon@2.5.0/fonts/remixicon.css" rel="stylesheet"></link>
+      <link href="https://emoji-css.afeld.me/emoji.css" rel="stylesheet"></link>
       <h1>Connect Four</h1>
       <h2>Project Information</h2>
-      <a href="https://ianchan3.github.io/Connect-Four/" target="blank">Project Link</a>
+      <a href="https://ianchan3.github.io/Connect-Four/" target="blank" class="icon icon--link">
+      <i class="em em-link" ></i>
+      </a>
       <br/>
-      <a href="https://github.com/ianchan3/Connect-Four" target="blank">Github Link</a>
+      <a href="https://github.com/ianchan3/Connect-Four" target="blank" class="icon icon--github">
+        <i class="ri-github-line"></i>
+      </a>
     </main>
   )
-}
+  }
--- src/pages/ProjectPage/ProjectPage.jsx
+++ src/pages/ProjectPage/ProjectPage.jsx
@@ -5,10 +5,13 @@ export default function ProjectPage () {
   return (
     <main class="ProjectPage">
       <h1>Projects</h1>
+      <body>
       <Link to="/projects/ConnectFour">Connect Four</Link>
       <Link to="/projects/FeelTheHeat">Feel The Heat?</Link>
       <Link to="/projects/LoveStruck">LoveStruck</Link>
       <Link to="/projects/GrowGreen">Grow Green</Link>
+     
+      </body>
     </main>
   )
 }
