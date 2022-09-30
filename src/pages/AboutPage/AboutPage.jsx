678290e1572687e97d3790c2ac885092197a0973
ianchan3 <ianchan3138@gmail.com>
2022-09-30 16:32:56 -0700

    Technologies Used

--- src/pages/AboutPage/AboutPage.jsx
+++ src/pages/AboutPage/AboutPage.jsx
@@ -5,57 +5,33 @@ export default function AboutPage() {
     <main class="AboutPage">
       <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/devicons/devicon@v2.15.1/devicon.min.css"></link>
       <h1>Hi 👋, I'm Ian!</h1>
-      <h2>I'm a Passionate Full Stack Developer </h2>
-      <ul>Skills
-        <div>HTML
-          <br/>
-          <i class="devicon-html5-plain colored"></i>
-        </div>
-        <div>CSS
-          <br/>
-          <i class="devicon-css3-plain colored"></i>
-        </div>
-        <div>JavaScript
-          <br/>
-          <i class="devicon-javascript-plain colored"></i>
-        </div>
-        <div>MongoDB
-          <br/>
-          <i class="devicon-mongodb-plain colored"></i>
-        </div>
-        <div>Express.js
-          <br/>
-          <i class="devicon-express-original colored"></i>
-        </div>
-        <div>React
-          <br/>
-          <i class="devicon-react-original colored"></i>
-        </div>
-        <div>Node.js
-          <br/>
-          <i class="devicon-nodejs-plain colored"></i>
-        </div>
-        <div>Python
-          <br/>
-          <i class="devicon-python-plain colored"></i>
-        </div>
-        <div>Django
-          <br/>
-          <i class="devicon-django-plain colored"></i>
-        </div>
-        <div>PostgreSQL
-          <br/>
-          <i class="devicon-postgresql-plain colored"></i>
-        </div>
-        <div>Github
-          <br/>
-          <i class="devicon-github-original colored"></i>
-        </div>
-        <div>Heroku
-          <br/>
-          <i class="devicon-heroku-original colored"></i>
-        </div>
-      </ul>
+      <h2>I'm a Passionate Full Stack Developer! </h2>
+      <span class="technologies">
+        <i class="devicon-html5-plain colored"></i>
+        <i class="devicon-css3-plain colored"></i>
+        <i class="devicon-javascript-plain colored"></i>
+        <i class="devicon-github-original colored"></i>
+        <h1 class="skills">HTML</h1>
+        <h1 class="skills">CSS</h1>
+        <h1 class="skills">JavaScript</h1>
+        <h1 class="skills">Github</h1>
+        <i class="devicon-mongodb-plain colored"></i>
+        <i class="devicon-express-original colored"></i>
+        <i class="devicon-react-original colored"></i>
+        <i class="devicon-nodejs-plain colored"></i>
+        <h1 class="skills">MongoDB</h1>
+        <h1 class="skills">Express.js</h1>
+        <h1 class="skills">React</h1>
+        <h1 class="skills">Node.js</h1>
+        <i class="devicon-python-plain colored"></i>
+        <i class="devicon-django-plain colored"></i>
+        <i class="devicon-postgresql-plain colored"></i>
+        <i class="devicon-heroku-original colored"></i>
+        <h1 class="skills">Python</h1>
+        <h1 class="skills">Django</h1>
+        <h1 class="skills">PostgreSQL</h1>
+        <h1 class="skills">Heroku</h1>
+        </span>    
     </main>
   )
 }
