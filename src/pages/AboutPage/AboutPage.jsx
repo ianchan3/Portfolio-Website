093a4e2431cce17f82e10f4a12b7c3b4fb586501
ianchan3 <ianchan3138@gmail.com>
2022-09-30 14:45:10 -0700

    Programming Languages and Skills Add

--- src/pages/AboutPage/AboutPage.jsx
+++ src/pages/AboutPage/AboutPage.jsx
@@ -3,8 +3,59 @@ import "./AboutPage.css";
 export default function AboutPage() {
   return (
     <main class="AboutPage">
-      <h1>Hello, my name is Ian!</h1>
-      <h2>I'm a full stack developer </h2>
+      <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/devicons/devicon@v2.15.1/devicon.min.css"></link>
+      <h1>Hi 👋, I'm Ian!</h1>
+      <h2>I'm a Passionate Full Stack Developer </h2>
+      <ul>Skills
+        <div>HTML
+          <br/>
+          <i class="devicon-html5-plain colored"></i>
+        </div>
+        <div>CSS
+          <br/>
+          <i class="devicon-css3-plain colored"></i>
+        </div>
+        <div>JavaScript
+          <br/>
+          <i class="devicon-javascript-plain colored"></i>
+        </div>
+        <div>MongoDB
+          <br/>
+          <i class="devicon-mongodb-plain colored"></i>
+        </div>
+        <div>Express.js
+          <br/>
+          <i class="devicon-express-original colored"></i>
+        </div>
+        <div>React
+          <br/>
+          <i class="devicon-react-original colored"></i>
+        </div>
+        <div>Node.js
+          <br/>
+          <i class="devicon-nodejs-plain colored"></i>
+        </div>
+        <div>Python
+          <br/>
+          <i class="devicon-python-plain colored"></i>
+        </div>
+        <div>Django
+          <br/>
+          <i class="devicon-django-plain colored"></i>
+        </div>
+        <div>PostgreSQL
+          <br/>
+          <i class="devicon-postgresql-plain colored"></i>
+        </div>
+        <div>Github
+          <br/>
+          <i class="devicon-github-original colored"></i>
+        </div>
+        <div>Heroku
+          <br/>
+          <i class="devicon-heroku-original colored"></i>
+        </div>
+      </ul>
     </main>
   )
 }
