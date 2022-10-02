a8831917be9cfc5dac5e403f6cecdab80d6789b2
ianchan3 <ianchan3138@gmail.com>
2022-10-02 15:17:34 -0700

    CSS Changes and Background Picture Changes

--- src/pages/GrowGreenPage/GrowGreenPage.jsx
+++ src/pages/GrowGreenPage/GrowGreenPage.jsx
@@ -3,7 +3,7 @@ import "./GrowGreenPage.css"
 export default function GrowGreenPage() {
   return (
     <main className="GrowGreenPage">
-            <link href="https://cdn.jsdelivr.net/npm/remixicon@2.5.0/fonts/remixicon.css" rel="stylesheet"></link>
+      <link href="https://cdn.jsdelivr.net/npm/remixicon@2.5.0/fonts/remixicon.css" rel="stylesheet"></link>
       <link href="https://emoji-css.afeld.me/emoji.css" rel="stylesheet"></link>
       <h1>Grow Green </h1>
       <h2>Project Information</h2>
