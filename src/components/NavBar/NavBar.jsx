52cb792c606986627fded45a6e064ec83c49eea0
ianchan3 <ianchan3138@gmail.com>
2023-02-16 22:34:13 -0800

    CSS Changes for Day Night Toggle

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -7,14 +7,14 @@ export default function NavBar() {
     <main className="NavBar">
       <link href="https://cdn.jsdelivr.net/npm/remixicon@2.5.0/fonts/remixicon.css" rel="stylesheet"></link>
       <nav>
-        <li><a href="#" id="logo"><Logo /></a></li>
+        <li className='logo'><a href="#" id="logo"><Logo /></a></li>
         <li><a href="#two" id="navbar">Technologies</a></li>
         <li><a href="#three" id="navbar">Projects</a></li>
         <a href="https://drive.google.com/file/d/1ka4cuzDJfVQzdXGvvTr48adBIkUgSzo_/view?usp=sharing" target="blank" id="navbar" class="resume">Resume</a>
-        <a href="https://github.com/ianchan3/" target="blank" id="icon"class="icon icon--github">
+        <a href="https://github.com/ianchan3/" target="blank" id="icon"class="icon icon--github--navbar">
           <i id="icon" class="ri-github-line"></i>
         </a>
-        <a href="https://www.linkedin.com/in/ianchan-/" target= "blank" id="icon"class="icon icon--linkedin">
+        <a href="https://www.linkedin.com/in/ianchan-/" target= "blank" id="icon"class="icon icon--linkedin--navbar">
           <i id="icon" class="ri-linkedin-line"></i>
         </a>
         <DayNightFeature />
--- src/components/NavBarReturn/NavBarReturn.jsx
+++ src/components/NavBarReturn/NavBarReturn.jsx
@@ -1,3 +1,4 @@
+import "./NavBarReturn.css";
 import { Link } from 'react-router-dom';
 import Logo from '../Logo/Logo';
 import DayNightFeature from '../DayNightFeature/DayNightFeature';
@@ -8,14 +9,14 @@ export default function NavBarReturn() {
     <main className="NavBar">
       <link href="https://cdn.jsdelivr.net/npm/remixicon@2.5.0/fonts/remixicon.css" rel="stylesheet"></link>
       <nav>
-        <Link to ="/"><Logo /></Link>
+        <Link to ="/" className="logo"><Logo /></Link>
         <Link to ="/#two" id="navbar" class="about">About Me</Link>
         <Link to ="/#three" id="navbar">Projects</Link>
         <a href="https://drive.google.com/file/d/1c-psynSp7Jkzdxnr6Cq7Hd-Wow2ICx8a/view?usp=sharing" target="blank" id="navbar" class="resume">Resume</a>
-        <a href="https://github.com/ianchan3/" target="blank" id="navbar"class="icon icon--github">
+        <a href="https://github.com/ianchan3/" target="blank" id="navbar"class="icon icon--github--navbarreturn">
           <i id="navbar" class="ri-github-line"></i>
         </a>
-        <a href="https://www.linkedin.com/in/ianchan-/" target= "blank" id="navbar"class="icon icon--linkedin">
+        <a href="https://www.linkedin.com/in/ianchan-/" target= "blank" id="navbar"class="icon icon--linkedin--navbarreturn">
           <i id="navbar" class="ri-linkedin-line"></i>
         </a>
         <DayNightFeature />
