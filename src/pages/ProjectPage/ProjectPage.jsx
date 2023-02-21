cd7cc68f48889499fc5b519522b50edd837a3763
ianchan3 <ianchan3138@gmail.com>
2023-02-21 15:04:32 -0800

    Loading Bar More Visible and Animation on Scroll Implemented

--- src/pages/ProjectPage/ProjectPage.jsx
+++ src/pages/ProjectPage/ProjectPage.jsx
@@ -1,18 +1,24 @@
 import "./ProjectPage.css"
 import { Link } from "react-router-dom";
+import AOS from 'aos';
+import 'aos/dist/aos.css'; 
 
 export default function ProjectPage() {
+  AOS.init({
+    duration: 1000,
+  });
+
   return (
     <section id="three">
       <main className="ProjectPage">
         <h1>Projects</h1>
         <p>
-          <Link to="/projects/TExpress"><img src="https://i.imgur.com/oXqGh1z.png" alt="Calculator App"></img></Link>
-          <Link to="/projects/FeelTheHeat"><img src="https://i.imgur.com/4Cw2lUX.jpg" alt="Feel The Heat?"></img></Link>
-          <Link to="/projects/Calculator"><img src="https://i.imgur.com/VtIlnQW.jpg" alt="Calculator App"></img></Link>
-          <Link to="/projects/ConnectFour"><img src="https://i.imgur.com/ZaLPJld.jpg" alt="Connect Four"></img></Link>
-          <Link to="/projects/GrowGreen"><img src="https://i.imgur.com/PCvd6LZ.png" alt="Grow Green"></img></Link>
-          <Link to="/projects/LoveStruck"><img src="https://i.imgur.com/W3Mikdl.png" alt="LoveStruck"></img></Link>
+          <Link to="/projects/TExpress"><img src="https://i.imgur.com/oXqGh1z.png" alt="T Express" data-aos="fade-left"></img></Link>
+          <Link to="/projects/FeelTheHeat"><img src="https://i.imgur.com/4Cw2lUX.jpg" alt="Feel The Heat?" data-aos="flip-left"></img></Link>
+          <Link to="/projects/Calculator"><img src="https://i.imgur.com/VtIlnQW.jpg" alt="Calculator App" data-aos="fade-right"></img></Link>
+          <Link to="/projects/ConnectFour"><img src="https://i.imgur.com/ZaLPJld.jpg" alt="Connect Four" data-aos="fade-left"></img></Link>
+          <Link to="/projects/GrowGreen"><img src="https://i.imgur.com/PCvd6LZ.png" alt="Grow Green" data-aos="flip-right"></img></Link>
+          <Link to="/projects/LoveStruck"><img src="https://i.imgur.com/W3Mikdl.png" alt="LoveStruck" data-aos="fade-right"></img></Link>
         </p>
       </main>
     </section>
