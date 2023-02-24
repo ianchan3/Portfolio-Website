d0bb767eef98a945f16beed8b53ac8eb2bc8688b
ianchan3 <ianchan3138@gmail.com>
2023-02-24 11:54:53 -0800

    T Express Experience Description Updated

--- src/pages/ProjectPage/ProjectPage.jsx
+++ src/pages/ProjectPage/ProjectPage.jsx
@@ -11,7 +11,7 @@ export default function ProjectPage() {
   return (
     <section id="three">
       <main className="ProjectPage">
-        <h1>PROJECTS</h1>
+        <h1>PROJECTS & EXPERIENCE</h1>
         <div className="horizontal-line-effect left"></div>
         <div className="horizontal-line-effect right"></div>
         <p>
@@ -22,7 +22,7 @@ export default function ProjectPage() {
             <span>Logistics Website</span>
             <br></br>
             <span>Vue | Node | Sequelize | Bootstrap</span>
-            <h3 className="project-info">T Express is a multiservice logistic and transport company that sends shipment overseas to East Asia.</h3>
+            <h3 className="project-info">Currently, I am interning at T Express, a logistics and transportation company that ships packages to East Asia. As part of my role, I assist in implementing new features and fixing software bugs within the Vue, Sequelize, and BootStrap application. I have experience in developing product features from initial design documentation to final implementation, testing, and deployment. </h3>
           </div>
           <Link to="/projects/FeelTheHeat"><img src="https://i.imgur.com/4Cw2lUX.jpg" alt="Feel The Heat?" data-aos="flip-right"></img></Link>
           <div className="project-info-container" data-aos="fade-left">
