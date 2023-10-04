f8112da6b73ddc2ce67b209917991c422f5fbe74
ianchan3 <ianchan3138@gmail.com>
2023-10-03 21:57:47 -0700

    Resume Link Changed

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -2,7 +2,7 @@ import './Logo.css';
 
 export default function Logo() {
   return (
-    <div title="Home" aria-label="Home" className="Logo">
+    <div title="Home" aria-label="Home Link" className="Logo">
       <h1>IC</h1>
     </div>
   );
